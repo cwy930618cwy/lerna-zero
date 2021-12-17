7141ef502b8e2d4ffcba01c8aea10232b0a8f706
cwy <834415728@qq.com>
2021-12-17 14:43:17 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# lerna-zero
-simple-lerna
+lerna-zero
---
+++ packages/moduleA/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <div class="text">
+      home
+    </div>
+      <el-button icon="el-icon-question" type="primary" >
+      Show Guide
+    </el-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style scoped>
+.home {
+  color: rebeccapurple;
+}
+.text {
+  color: green;
+}
+</style>
